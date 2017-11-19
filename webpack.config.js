48100390a7e11f6304610786df3a529568ac9adb
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2017-11-19 23:20:36 +0800

    enabled hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const devServer = require('webpack-dev-server');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
@@ -14,11 +15,12 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.scss$/, 
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: ["css-loader", "sass-loader"]
-            })
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"] 
+            // use: ExtractTextPlugin.extract({
+            //     fallback: "style-loader",
+            //     use: ["css-loader", "sass-loader"]
+            // })
         }, {
             test: [/\.js$/, /\.jsx$/],
             use: 'babel-loader',
@@ -31,6 +33,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname + '/dist'),
         compress: true,
+        hot: true,
         port: 9000,
         // stats: 'errors-only'
     },
@@ -53,8 +56,10 @@ module.exports = {
         }),
         new ExtractTextPlugin({
             filename: "app.css",
-            disable: false,
+            disable: true,
             allChunks: true
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
